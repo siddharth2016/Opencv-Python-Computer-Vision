2cf1e8186edde71b4864fe96b522db7f8bf312b2
siddharth2016 <siddharthchandragzb@gmail.com>
2021-04-17 13:37:48 +0530

    update structure of code

--- openCvSymbolDrawing.py
+++ openCvSymbolDrawing.py
@@ -12,9 +12,11 @@ img = cv2.ellipse(img, (176, 200), (60,60), 0,0,300,(0,255,0),-1)
 img = cv2.ellipse(img, (176, 200), (20,20), 0,0,300,(255,255,255),-1)
 img = cv2.ellipse(img, (336, 200), (60,60), 300,0,300,(255,0,0),-1)
 img = cv2.ellipse(img, (336, 200), (20,20), 300,0,300,(255,255,255),-1)
+
 font = cv2.FONT_HERSHEY_SIMPLEX
 img = cv2.putText(img, "OpenCV", (196,296), font, 1, (0,0,0), 4, cv2.LINE_AA)
-cv2.imwrite('opencvlogo.png', img)
+
 cv2.imshow("OpenCV Logo", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+cv2.imwrite('opencvlogo.png', img)
