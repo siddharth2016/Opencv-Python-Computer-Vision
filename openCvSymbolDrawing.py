397cfd538bedee3abc0fb0443e15069c2d46428a
siddharth2016 <siddharthchandragzb@gmail.com>
2021-04-17 11:23:38 +0530

    update positions and img size

--- openCvSymbolDrawing.py
+++ openCvSymbolDrawing.py
@@ -4,32 +4,17 @@ import cv2
 import numpy as np
 
 #output image
-img = np.zeros((512,512,3), dtype="uint8")
-
-##cv2.imshow("img",img)
-##cv2.waitKey(0)
-##cv2.destroyAllWindows()
-
-##img = cv2.circle(img, (256,80), 60, (0,0,255), -1)
-##img = cv2.circle(img, (256, 80), 20, (0,0,0), -1)
-##img = cv2.circle(img, (176, 200), 60, (0,255,0), -1)
-##img = cv2.circle(img, (176, 200), 20, (0,0,0), -1)
-##img = cv2.circle(img, (336, 200), 60, (255,0,0), -1)
-##img = cv2.circle(img, (336, 200), 20, (0,0,0), -1)
-##font = cv2.FONT_HERSHEY_SIMPLEX
-##img = cv2.putText(img, "OpenCV", (196,296), font, 1, (255,255,255))
-##cv2.imshow("img",img)
-##cv2.waitKey(0)
-##cv2.destroyAllWindows()
+img = np.full((360, 512, 3), 255, dtype="uint8")
 
 img = cv2.ellipse(img, (256, 80), (60,60), 120,0,300,(0,0,255),-1)
-img = cv2.ellipse(img, (256, 80), (20,20), 120,0,300,(0,0,0),-1)
+img = cv2.ellipse(img, (256, 80), (20,20), 120,0,300,(255,255,255),-1)
 img = cv2.ellipse(img, (176, 200), (60,60), 0,0,300,(0,255,0),-1)
-img = cv2.ellipse(img, (176, 200), (20,20), 0,0,300,(0,0,0),-1)
+img = cv2.ellipse(img, (176, 200), (20,20), 0,0,300,(255,255,255),-1)
 img = cv2.ellipse(img, (336, 200), (60,60), 300,0,300,(255,0,0),-1)
-img = cv2.ellipse(img, (336, 200), (20,20), 300,0,300,(0,0,0),-1)
+img = cv2.ellipse(img, (336, 200), (20,20), 300,0,300,(255,255,255),-1)
 font = cv2.FONT_HERSHEY_SIMPLEX
-img = cv2.putText(img, "OpenCV", (196,296), font, 1, (255,255,255), 4, cv2.LINE_AA)
-cv2.imshow("img", img)
+img = cv2.putText(img, "OpenCV", (196,296), font, 1, (0,0,0), 4, cv2.LINE_AA)
+cv2.imwrite('opencvlogo.png', img)
+cv2.imshow("OpenCV Logo", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
