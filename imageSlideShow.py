bbd39630a375f5a6a7d7e786da54092a6434222b
siddharth2016 <siddharthchandragzb@gmail.com>
2021-04-25 13:46:05 +0530

    update code

--- imageSlideShow.py
+++ imageSlideShow.py
@@ -2,35 +2,40 @@
 
 import cv2
 import numpy as np
+from math import ceil
 import os
 
 # Getting images from image base
-dst = "/home/siddharth/Desktop/PROGRAMS/Work/Opencv exercise/ 0. Image Base/"
+dst = "./images/"
 images = os.listdir(dst)
 length = len(images)
 
-#print(images)
-
-result = cv2.imread(dst + images[0])
+result = np.zeros((360,360,3), np.uint8)
 i = 1
 count = 0
+a = 1.0     # alpha
+b = 0.0     # beta
+img = cv2.imread(dst + images[i])
+img = cv2.resize(img, (360, 360))
 
 # Slide Show Loop
 while(True):
-    a = 1.0
-    b = 0.0
-    img = cv2.imread(dst + images[i])
-    
-    # Image Transition from one to another
-    while(int(b)!=1):
-        result = cv2.addWeighted(result, a, img, b, 0)
-        cv2.imshow("slideShow", result)
-        a -= 0.0001
-        b += 0.0001
-        
-    i = (i+1)%length    # Getting new image from directory
 
-    if cv2.waitKey(1) & 0xff == ord('q'):
+    if(ceil(a)==0):
+        a = 1.0
+        b = 0.0
+        i = (i+1)%length    # Getting new image from directory
+        img = cv2.imread(dst + images[i])
+        img = cv2.resize(img, (360, 360))
+
+    a -= 0.01
+    b += 0.01
+
+    # Image Transition from one to another
+    result = cv2.addWeighted(result, a, img, b, 0)
+    cv2.imshow("Slide Show", result)
+    key = cv2.waitKey(1) & 0xff
+    if key==ord('q'):
         break
 
 cv2.destroyAllWindows()
